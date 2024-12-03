dd3963b8ab6a23b8def459e0aa6e52852860e53a
Manjari14 <sheoranmanjari@gmail.com>
2024-12-04 01:25:35 +0530

    changes

--- gweccc-frontend/src/api.js
+++ gweccc-frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000/api";
 
 export const registerParticipant = async (data) => {
   const response = await axios.post(`${API_BASE_URL}/register`, data);
