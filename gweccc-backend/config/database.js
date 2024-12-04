2e50e1906a4d593751fed303f9fc12868269917a
Manjari14 <sheoranmanjari@gmail.com>
2024-12-04 16:50:21 +0530

    mongo connectuon

--- gweccc-backend/config/database.js
+++ gweccc-backend/config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config({ path: "./gweccc-backend/.env" });
 
 const connectDB = async () => {
   try {
+    console.log("Environment Variables Loaded:", process.env); // Debugging line
+
+    console.log("MONGO_URI:", process.env.MONGO_URI); // Debugging line
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
